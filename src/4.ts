f692bcc3ff86a0cb67c7b4d40c7d0ebc11fdedd9
Oleg998 <137883971+Oleg998@users.noreply.github.com>
2024-03-03 23:05:08 +0200

    done

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,62 @@
-const key = new Key();
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
 
-const house = new MyHouse(key);
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: Key;
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  protected key: Key;
+  protected tenants: Person[] = [];
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log(`${person.getKey().getSignature()} entered the house.`);
+    } else {
+      console.log("The door is closed.");
+    }
+  }
+
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("The door is open.");
+    } else {
+      console.log("Invalid key.");
+    }
+  }
+}
+
+const key = new Key();
 const person = new Person(key);
 
-house.openDoor(person.getKey());
+const myHouse = new MyHouse(key);
 
-house.comeIn(person);
+myHouse.openDoor(key);
 
 
-export {};
+myHouse.comeIn(person); 
